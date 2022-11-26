b141fa3ffc216f70fc8ee6c8aa214f7e87bce267
lucifiel0121 <zyi3204@gmail.com>
2022-11-26 23:43:01 +0800

    refactor(frontend): remove home reducer "title" state

--- frontend/src/reducers/home.js
+++ frontend/src/reducers/home.js
@@ -1,7 +1,6 @@
 import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
-  CHANGE_TITLE_FILTER,
 } from "../constants/actionTypes";
 
 const reducer = (state = {}, action) => {
@@ -13,11 +12,6 @@ const reducer = (state = {}, action) => {
       };
     case HOME_PAGE_UNLOADED:
       return {};
-    case CHANGE_TITLE_FILTER:
-      return {
-        ...state,
-        title: action.title,
-      };
     default:
       return state;
   }
